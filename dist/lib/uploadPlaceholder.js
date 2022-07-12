83a28769dcb7f3654971e00123214d3254d443e6
Praveen Venigalla <104462593+pvenigalla-pbsc@users.noreply.github.com>
2022-07-12 15:20:44 -0400

    fix upload place holder import

--- dist/lib/uploadPlaceholder.js
+++ dist/lib/uploadPlaceholder.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const prosemirror_view_1 = require("prosemirror-view");
 const isVideo_1 = __importDefault(require("../queries/isVideo"));
-const Plugin = require('prosemirror-state').Plugin;
+const { Plugin } = require('prosemirror-state').default;
 const uploadPlaceholder = new Plugin({
     state: {
         init() {
--- src/lib/uploadPlaceholder.ts
+++ src/lib/uploadPlaceholder.ts
@@ -1,7 +1,7 @@
 import { Decoration, DecorationSet } from "prosemirror-view";
 import isVideo from "../queries/isVideo";
 
-const Plugin = require('prosemirror-state').Plugin;
+const {Plugin} = require('prosemirror-state').default;
 
 
 // based on the example at: https://prosemirror.net/examples/upload/
