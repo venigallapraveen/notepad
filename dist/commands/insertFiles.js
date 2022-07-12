af63069e78072226343bd2dcbd16f5aec5cb1289
Praveen Venigalla <104462593+pvenigalla-pbsc@users.noreply.github.com>
2022-07-12 17:04:56 -0400

    fix upload place holder import

--- dist/commands/insertFiles.js
+++ dist/commands/insertFiles.js
@@ -5,51 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const isVideo_1 = __importDefault(require("../queries/isVideo"));
 const types_1 = require("../types");
-const prosemirror_state_1 = require("prosemirror-state");
-const prosemirror_view_1 = require("prosemirror-view");
+const uploadPlaceholder_1 = require("../lib/uploadPlaceholder");
 const insertFiles = function (view, event, pos, files, options) {
-    const uploadPlaceholderPlugin = new prosemirror_state_1.Plugin({
-        state: {
-            init() {
-                return prosemirror_view_1.DecorationSet.empty;
-            },
-            apply(tr, set) {
-                set = set.map(tr.mapping, tr.doc);
-                const action = tr.getMeta(this);
-                if (action && action.add) {
-                    const element = document.createElement("div");
-                    element.className = "image placeholder";
-                    const isFileVideo = isVideo_1.default(action.add.file.name);
-                    const media = document.createElement(isFileVideo ? "video" : "img");
-                    media.src = URL.createObjectURL(action.add.file);
-                    if (isFileVideo) {
-                        media.style.width = "100%";
-                        media.style.height = "100%";
-                        media.controls = true;
-                    }
-                    element.appendChild(media);
-                    const deco = prosemirror_view_1.Decoration.widget(action.add.pos, element, {
-                        id: action.add.id,
-                    });
-                    set = set.add(tr.doc, [deco]);
-                }
-                else if (action && action.remove) {
-                    set = set.remove(set.find(null, null, (spec) => spec.id === action.remove.id));
-                }
-                return set;
-            },
-        },
-        props: {
-            decorations(state) {
-                return this.getState(state);
-            },
-        },
-    });
-    const findPlaceholder = (state, id) => {
-        const decos = uploadPlaceholderPlugin.getState(state);
-        const found = decos.find(null, null, (spec) => spec.id === id);
-        return found.length ? found[0].from : null;
-    };
     const images = files.filter((file) => /image/i.test(file.type));
     const videos = files.filter((file) => /video/i.test(file.type));
     if (images.length === 0 && videos.length === 0)
@@ -68,7 +25,7 @@ const insertFiles = function (view, event, pos, files, options) {
     for (const file of validFile) {
         const id = {};
         const { tr } = view.state;
-        tr.setMeta(uploadPlaceholderPlugin, {
+        tr.setMeta(uploadPlaceholder_1.uploadPlaceholderPlugin, {
             add: { id, file, pos },
         });
         view.dispatch(tr);
@@ -79,12 +36,12 @@ const insertFiles = function (view, event, pos, files, options) {
                 ? document.createElement("video")
                 : new Image();
             const initFileLoad = () => {
-                const pos = findPlaceholder(view.state, id);
+                const pos = uploadPlaceholder_1.findPlaceholder(view.state, id);
                 if (pos === null)
                     return;
                 const transaction = view.state.tr
                     .replaceWith(pos, pos, schema.nodes.image.create({ src }))
-                    .setMeta(uploadPlaceholderPlugin, { remove: { id } });
+                    .setMeta(uploadPlaceholder_1.uploadPlaceholderPlugin, { remove: { id } });
                 view.dispatch(transaction);
             };
             if (isFileVideo) {
@@ -102,7 +59,7 @@ const insertFiles = function (view, event, pos, files, options) {
         })
             .catch((error) => {
             console.error(error);
-            const transaction = view.state.tr.setMeta(uploadPlaceholderPlugin, {
+            const transaction = view.state.tr.setMeta(uploadPlaceholder_1.uploadPlaceholderPlugin, {
                 remove: { id },
             });
             view.dispatch(transaction);
--- dist/nodes/Image.js
+++ dist/nodes/Image.js
@@ -29,10 +29,9 @@ const prosemirror_inputrules_1 = require("prosemirror-inputrules");
 const styled_components_1 = __importDefault(require("styled-components"));
 const react_medium_image_zoom_1 = __importDefault(require("react-medium-image-zoom"));
 const getDataTransferFiles_1 = __importDefault(require("../lib/getDataTransferFiles"));
+const uploadPlaceholder_1 = require("../lib/uploadPlaceholder");
 const insertFiles_1 = __importDefault(require("../commands/insertFiles"));
 const Node_1 = __importDefault(require("./Node"));
-const prosemirror_view_1 = require("prosemirror-view");
-const isVideo_1 = __importDefault(require("../queries/isVideo"));
 const IMAGE_INPUT_REGEX = /!\[(?<alt>[^\]\[]*?)]\((?<filename>[^\]\[]*?)(?=\“|\))\“?(?<layoutclass>[^\]\[\”]+)?\”?\)$/;
 const uploadPlugin = (options) => new prosemirror_state_1.Plugin({
     props: {
@@ -346,44 +345,7 @@ class Image extends Node_1.default {
         ];
     }
     get plugins() {
-        const uploadPlaceholderPlugin = new prosemirror_state_1.Plugin({
-            state: {
-                init() {
-                    return prosemirror_view_1.DecorationSet.empty;
-                },
-                apply(tr, set) {
-                    set = set.map(tr.mapping, tr.doc);
-                    const action = tr.getMeta(this);
-                    if (action && action.add) {
-                        const element = document.createElement("div");
-                        element.className = "image placeholder";
-                        const isFileVideo = isVideo_1.default(action.add.file.name);
-                        const media = document.createElement(isFileVideo ? "video" : "img");
-                        media.src = URL.createObjectURL(action.add.file);
-                        if (isFileVideo) {
-                            media.style.width = "100%";
-                            media.style.height = "100%";
-                            media.controls = true;
-                        }
-                        element.appendChild(media);
-                        const deco = prosemirror_view_1.Decoration.widget(action.add.pos, element, {
-                            id: action.add.id,
-                        });
-                        set = set.add(tr.doc, [deco]);
-                    }
-                    else if (action && action.remove) {
-                        set = set.remove(set.find(null, null, (spec) => spec.id === action.remove.id));
-                    }
-                    return set;
-                },
-            },
-            props: {
-                decorations(state) {
-                    return this.getState(state);
-                },
-            },
-        });
-        return [uploadPlaceholderPlugin, uploadPlugin(this.options)];
+        return [uploadPlaceholder_1.uploadPlaceholderPlugin, uploadPlugin(this.options)];
     }
 }
 exports.default = Image;
